df370795d54ea2c421c4bba47d9cfa57b734e9d8
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-05 17:43:43 -0700

    Update FizzBuzz#compute() to compute a range of numbers

--- test/fizzBuzzTest.js
+++ test/fizzBuzzTest.js
@@ -2,11 +2,9 @@ var FizzBuzz = require('../lib/fizzBuzz');
 var expect = require('chai').expect;
 
 describe('A FizzBuzz instance', function() {
-  it('should compute fizzbuzz for a given number', function() {
+  it('should compute fizzbuzz for a given range', function() {
     var fb = new FizzBuzz();
-    expect(fb.compute(2)).to.equal('2');
-    expect(fb.compute(3)).to.equal('fizz');
-    expect(fb.compute(5)).to.equal('buzz');
-    expect(fb.compute(15)).to.equal('fizzbuzz');
+    result = "buzz\n11\nfizz\n13\n14\nfizzbuzz\n"
+    expect(fb.compute(10, 15)).to.equal(result);
   });
 });
